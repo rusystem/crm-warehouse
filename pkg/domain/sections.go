dfef7c434f76dc33a76f8cc6698aa1b0b4b79664
mdmitry <ru.system.ru@gmail.com>
2024-11-23 12:53:19 +0500

    add client for responsible person

--- pkg/domain/sections.go
+++ pkg/domain/sections.go
@@ -1,13 +1,13 @@
 package domain
 
 const (
-	SectionFullAllAccess            = "full_all_access"
-	SectionFullCompanyAccess        = "full_company_access"
-	SectionFullAccess               = "full_access"
-	SectionOrderCardAccess          = "order_card_access"
-	SectionProductionDataAccess     = "production_data_access"
-	SectionStatusAndCalculateAccess = "status_and_calculate_access"
-	SectionPurchasePlanningAccess   = "purchase_planning_access"
+	SectionFullAllAccess            = "full_all_access"             // полный доступ
+	SectionFullCompanyAccess        = "full_company_access"         // полный доступ по управлению в рамках компании
+	SectionFullAccess               = "full_access"                 // Начальник цеха, Зам начальника цеха, Просчетчик. Доступ ко всем данным.
+	SectionOrderCardAccess          = "order_card_access"           // Конструктор.Доступ только к информации по карточке заказа: описание изделия, количество, уникальные номера заказа, ответственный менеджер, задачи по заказу.
+	SectionProductionDataAccess     = "production_data_access"      // Работник 1, Работник 2, Работник N. Доступ только к информации, необходимой для производства: описание изделия, количество, уникальные номера.
+	SectionStatusAndCalculateAccess = "status_and_calculate_access" // Менеджер. Доступ к статусу заказа и калькуляции.
+	SectionPurchasePlanningAccess   = "purchase_planning_access"    // Снабженец. Доступ к заявкам на закуп и планированию закупок.
 )
 
 type Section struct {
